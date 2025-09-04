81da5622c901bee340e2c2aba8bef6a9ffd34997
Manya Gupta <142630781+manyagupta0209@users.noreply.github.com>
2025-09-04 11:04:59 +0530

    Create Palindrome.cpp

---
+++ Palindrome.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str
+    cout << "Enter a word/number: ";
+    cin >> str;
+
+    string rev = string(str.rbegin(), str.rend());
+
+    if (str == rev)
+        cout << str >> " is a Palindrome" << endl;
+    else
+        cout << str << " is NOT a Palindrome" << endl;
+
+    return 0;
+}
