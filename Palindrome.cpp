0d0ddb7b811ab5a88f58d3161391a581f31173cd
Prakhar Gupta <prakhargupta.1092@gmail.com>
2025-09-04 11:25:56 +0530

    Update Palindrome.cpp

--- Palindrome.cpp
+++ Palindrome.cpp
@@ -2,14 +2,14 @@
 using namespace std;
 
 int main() {
-    string str
+    string str;
     cout << "Enter a word/number: ";
     cin >> str;
 
     string rev = string(str.rbegin(), str.rend());
 
     if (str == rev)
-        cout << str >> " is a Palindrome" << endl;
+        cout << str << " is a Palindrome" << endl;
     else
         cout << str << " is NOT a Palindrome" << endl;
 
