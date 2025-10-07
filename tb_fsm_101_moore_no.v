b26fe832e7d7271bf1263d4a7df94160a9ac2967
Prakhar Gupta <prakhargupta.1092@gmail.com>
2025-10-07 18:52:22 +0530

    Create tb_fsm_101_moore_no.v

---
+++ tb_fsm_101_moore_no.v
@@ -0,0 +1,21 @@
+module tb_fsm_101_moore_no;
+reg clk, rst,x;
+wire y;
+fsm_101_moore_no uut(.clk(clk),.rst(rst),.x(x),.y(y));
+always #2 clk=~clk;
+initial
+begin
+clk=0; rst=1;
+#70 rst=0;
+#10 x=1;
+#10 x=0;
+#10 x=1;
+#10 x=1;
+#10 x=0;
+#10 x=1;
+#10 x=0;
+#10 x=1;
+#10 x=1;
+#10 $finish;
+end
+endmodule
