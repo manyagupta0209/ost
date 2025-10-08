f4b0219cc673a160a5be6d3d87d5a403639f38d8
premish1806 <premishninawe@gmail.com>
2025-10-08 14:50:10 +0530

    Updated Design Code with new some correction

--- fsm_101_moore_no.v
+++ fsm_101_moore_no.v
@@ -3,7 +3,7 @@ input clk, rst, x;
 output reg y;
 reg q1, q0;
 reg d1, d0;
-always @(posedge clk)
+always @(posedge clk) //Solved the issue of postive edge clock
 begin
 if (rst)
 begin q1<= 1'b0; q0<=1'b0; end
@@ -13,9 +13,9 @@ end
 
 always @(*)
 begin
-d1 =((~q1)&(q0)&(~x))|(q1&(~q0)&x);
+d1 =((~q1)&(q0)&(~x))|(q1&(~q0)&x); //Logic Correction
 d0= x;
-y = q1&(~q0);
+y = q1&(~q0); //Equation calculation
 end
 endmodule
 //this is appended version using cat.
