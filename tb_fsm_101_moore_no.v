b80932d358909628a71555b53eb2563bef2255b8
premish1806 <premishninawe@gmail.com>
2025-10-08 15:00:29 +0530

    Updated Terminal Code with new some correction

--- tb_fsm_101_moore_no.v
+++ tb_fsm_101_moore_no.v
@@ -1,11 +1,11 @@
 module tb_fsm_101_moore_no;
 reg clk, rst,x;
 wire y;
-fsm_101_moore_no uut(.clk(clk),.rst(rst),.x(x),.y(y));
+fsm_101_moore_no uut(.clk(clk),.rst(rst),.x(x),.y(y)); //Previous Instantiation was wrong
 always #2 clk=~clk;
 initial
 begin
-clk=0; rst=1;
+clk=0; rst=1; //Added reset
 #70 rst=0;
 #10 x=1;
 #10 x=0;
